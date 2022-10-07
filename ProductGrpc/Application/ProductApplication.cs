2438a77ae9262c76f787c78c030b339530aa9021
Rodrigo Brito <rodrigo@w3ti.com.br>
2022-10-07 16:36:35 -0300

    Doc: Documentar classe de application para produto

--- ProductGrpc/Application/ProductApplication.cs
+++ ProductGrpc/Application/ProductApplication.cs
@@ -8,10 +8,27 @@ namespace ProductGrpc.Application
     {
 
         private readonly IProductRepository _repository;
-
         public ProductApplication(IProductRepository repository) => _repository = repository;
+
+        /// <summary>
+        /// Excluir um item
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
         public async Task DeletedAsync(Product product) => await _repository.DeleteAsync(product);
+
+        /// <summary>
+        /// Buscar todos os registros
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<Product>> GetAllAsync() => await _repository.GetAllAsync();
+
+        /// <summary>
+        /// Buscar registro pelo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException"></exception>
         public async Task<Product> GetByIdAsync(Guid id)
         {
             var product = await _repository.GetAsync(id);
@@ -20,6 +37,13 @@ namespace ProductGrpc.Application
 
             return product;
         }
+        
+        /// <summary>
+        /// Salvar um registro no banco
+        /// </summary>
+        /// <param name="product">Produto para salvar</param>
+        /// <param name="IsNewProduct">Eh registro novo?</param>
+        /// <returns></returns>
         public async Task<Product> SaveAsync(Product product, bool IsNewProduct = false)
         {
             if (IsNewProduct)
